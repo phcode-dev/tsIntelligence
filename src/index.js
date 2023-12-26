af0a2a3fd2d7ed4028fc54f4ffe52ffc71958db4
Charly Abraham <charly@core.ai>
2023-12-27 01:11:43 +0530

    feat: adding support for tsserver command definiton and find refernces

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import createTSServerInstance from './utils/startTsserver.js';
 
 const tsServer = createTSServerInstance();
+await tsServer.init();
+console.log('server intializeed');
+await tsServer.openFile( '/home/charly/repo/vscode/extensions/typescript-language-features/web/src/workerSession.ts');
+
+const definitonResp = await tsServer.getDefinition('/home/charly/repo/vscode/extensions/typescript-language-features/web/src/workerSession.ts', 26, 23)
+console.log("resp+++", JSON.stringify(definitonResp));
+
+const findReferencesResp = await tsServer.getDefinition('/home/charly/repo/vscode/extensions/typescript-language-features/web/src/workerSession.ts', 11, 16)
+console.log('find reference resp',JSON.stringify(findReferencesResp))
+//await tsServer.killServer();
+
 
 // Initialize tsserver
+/*
 tsServer.init()
     .then(() => {
         console.log('tsserver is ready');
@@ -23,3 +35,4 @@ tsServer.init()
     .catch(error => {
         console.error('Error:', error);
     });
+*/
